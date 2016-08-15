38b54ec989ad347ed422fcb10ba79d76f4a7477b
xchong <1074974408@qq.com>
2016-08-15 23:09:05 +0800

    add local-user

--- loginscript.py
+++ loginscript.py
@@ -2,20 +2,33 @@ import telnetlib,time,sys
 def conn(x):
   T=telnetlib.Telnet
   user='admin'  
-  pw='jxa618a'   
-  enpw=''  
+  pw='password'   
   tl=T(x)
+
   tl.read_until('Username:')
   tl.write(user+'\n')
+
   tl.read_until('Password:')
   tl.write(pw+'\n')
+  
+  print 'hi'
   tl.read_until('>')
   tl.write('system-view'+'\n')  
 #  cmd=['local-user admin','password cipher jxa618a']
+  
+  print 'hi'
   tl.read_until(']')
-  tl.write('local-user admin'+'\n')
+  tl.write('local-user admin2132'+'\n')
+ 
+  print 'hi'
   tl.read_until(']')
   tl.write('password cipher jxa618a'+'\n')
+  
+  tl.read_until(']')
+  tl.write('service-type telnet level 3'+'\n')
+
+  print tl.read_until(']')
+  print 'hi'
   tl.write('sa'+'\n') 
   tl.write('y'+'\n')
   tl.write('\n')
@@ -24,6 +37,6 @@ def conn(x):
   tl.close()
 f=open('ip.txt','r')
 for ip in f:
-  print ip   
-  
+  ip = ip.strip('\n')   
+  print ip 
   conn(ip)
